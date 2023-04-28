405a5042029d64a46d2b5240c71bc2f91cdeeccd
Lina H <f22linhe@student.his.se>
2023-04-28 10:58:06 +0200

    Kopplade ihop lokala variabler med mina TextViews i SecondActivity samt angav kod för att skriva ut datan i min bundle.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,19 +2,26 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView name, number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        name = findViewById(R.id.set_name);
+        number = findViewById(R.id.set_number);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String name = extras.getString("name");
             int number = extras.getInt("number");
         }
-
+        name.setText(extras.getString("name"));
+        number.setText(String.valueOf(extras.getInt("number")));
     }
 }
